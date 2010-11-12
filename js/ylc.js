900c78c46ea6d5bf92130b930b85b92cdaefee01
Rupert Redington <rupert@neontribe.co.uk>
2010-11-12 01:09:11 +0000

    Permalinking enabled via url fragments
    A bit ugly in places but seems to work. 
    
    Not tested outside firefox at present, and may be chancy
    in IE7 and earlier.

--- js/ylc.js
+++ js/ylc.js
@@ -1,4 +1,9 @@
 /**
+ * An event dispatching object
+ */
+var dispatcher = $({});
+
+/**
  * Parse out the rightmost element from a URI
  * Understands paths and fragments
  * 
@@ -6,7 +11,7 @@
  */
 function lastURIElement(uri) {
 	var lpe = uri.split('/').pop();
-return lpe.split('#').pop();
+	return lpe.split('#').pop();
 }
 
 /**
@@ -136,7 +141,7 @@ function groupOn(arr, key) {
  * 
  * returns true if the group ends up selected false if not
  * 
- * @param {Object} area
+ * @param {Object} group
  */
 function toggleGroupSelection(group) {
 	var pos = $.inArray(group, selected_groups);
@@ -151,20 +156,29 @@ function toggleGroupSelection(group) {
 	}
 	selected_groups = selected_groups.slice(-max_selected_groups);
 	
+	// Record the selected groups in the url fragment for permalinking
+	var groups_fragment = [];
+	$.each(selected_groups, function(i, g){
+		groups_fragment.push({'d':g.data.observations[0].dataset.value, 'g':g.data.observations[0][ol_group_by].value});
+	});
+	jQuery.bbq.pushState({'select_groups':groups_fragment});
+
+	
 	$.each(old_selection.concat(selected_groups), function(i, v){
 		var state = $.inArray(v, selected_groups) !== -1;
 		$.each(v.nodes, function(i, node){
 			updateSelectionUI(node, state);
 		});
 	});
-    
+	
+	
 	// Remove the hint once we've got one
-if (selected_groups.length === 0) {
-	$('#status').slideDown();
-$('#visualisation').addClass('empty');
-} else {
-	$('#status').slideUp();
-$('#visualisation').removeClass('empty');
+	if (selected_groups.length === 0) {
+		$('#status').slideDown();
+		$('#visualisation').addClass('empty');
+	} else {
+		$('#status').slideUp();
+		$('#visualisation').removeClass('empty');
 	}
 	
     drawComparison();
@@ -176,95 +190,94 @@ $('#visualisation').removeClass('empty');
    */
   function drawComparison() {
   	// Utility to make a label continaing age range and sex
-var dimension_key = function (obj) {
-    return getLabel(obj, 'drefAge') + ' ' + getLabel(obj, 'dsex');
-};
-var data = new google.visualization.DataTable();
-data.addColumn('string', 'Age Range');
-var measure = {};
+	var dimension_key = function (obj) {
+	    return getLabel(obj, 'drefAge') + ' ' + getLabel(obj, 'dsex');
+	};
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Age Range');
+	var measure = {};
 
-if (selected_groups.length > 0) {
-	//Find the measure by which to compare
-// Ask the user if we're not sure
-var measure = getMeasure(selected_groups[0].parent.data);
+	if (selected_groups.length > 0) {
+		//Find the measure by which to compare
+		// Ask the user if we're not sure
+		var measure = getMeasure(selected_groups[0].parent.data);
 
-// Loop over the data
-var stats = {cols:[], dimension:{}};
-$.each(selected_groups, function(i,v){
-    stats.cols.push(getLabel(v.data.observations[0], ol_group_by));
-    $.each(v.data.observations, function(i,v){
-        if (!stats.dimension.hasOwnProperty(dimension_key(v))) {
-            stats.dimension[dimension_key(v)] = {};
-        }
-        stats.dimension[dimension_key(v)][getLabel(v, ol_group_by)] = parseFloat(v[measure.ob_key].value);
-    });
-});
+		// Loop over the data
+		var stats = {cols:[], dimension:{}};
+		$.each(selected_groups, function(i,v){
+		    stats.cols.push(getLabel(v.data.observations[0], ol_group_by));
+		    $.each(v.data.observations, function(i,v){
+		        if (!stats.dimension.hasOwnProperty(dimension_key(v))) {
+		            stats.dimension[dimension_key(v)] = {};
+		        }
+		        stats.dimension[dimension_key(v)][getLabel(v, ol_group_by)] = parseFloat(v[measure.ob_key].value);
+		    });
+		});
 
-$.each(stats.cols, function(i, v) {
-    data.addColumn('number', v);
-});
+		$.each(stats.cols, function(i, v) {
+		    data.addColumn('number', v);
+		});
 
-var index = 0;
-$.each(stats.dimension, function(key, val){
-    data.addRows(1);
-    data.setValue(index, 0, key);
-    $.each(stats.cols, function(i,v){
-        data.setValue(index, i+1, val[v]);
-    });
-    index++;
-    
-});
+		var index = 0;
+		$.each(stats.dimension, function(key, val){
+		    data.addRows(1);
+		    data.setValue(index, 0, key);
+		    $.each(stats.cols, function(i,v){
+		        data.setValue(index, i+1, val[v]);
+		    });
+		    index++;  
+		});
 
-var chart = new google.visualization.ColumnChart(document.getElementById('visualisation'));
-chart.draw(data, {width: 600, height: 400, title: measure.title,
-               hAxis: {title: 'Age Range', titleTextStyle: {color: 'red'}}
-    });
-} else {
-	$('#visualisation').empty();
+		var chart = new google.visualization.ColumnChart(document.getElementById('visualisation'));
+		chart.draw(data, {width: 600, height: 400, title: measure.title,
+		               hAxis: {title: 'Age Range', titleTextStyle: {color: 'red'}}
+		});
+	} else {
+		$('#visualisation').empty();
 	}
- }
+}
   
-  /**
-   * Handle any UI state changes when a group is selected or deselected
-   * 
-   * @param {Object} button
-   * @param {Object} selected
-   */
-  function updateSelectionUI(elem, selected){
+/**
+ * Handle any UI state changes when a group is selected or deselected
+ * 
+ * @param {Object} button
+ * @param {Object} selected
+ */
+ function updateSelectionUI(elem, selected){
   	var elem = $(elem);
   	if (selected) {
 		elem.button( "option", "label", "remove" );
-elem.button('option', 'icons', {primary:'ui-icon-circle-minus'});
-elem.parents('.group_header:first').addClass('selected');
-} else {
-	elem.button( "option", "label", "add" );
-elem.button('option', 'icons', {primary:'ui-icon-circle-plus'});
-elem.parents('.group_header:first').removeClass('selected');
+		elem.button('option', 'icons', {primary:'ui-icon-circle-minus'});
+		elem.parents('.group_header:first').addClass('selected');
+  	} else {
+  		elem.button( "option", "label", "add" );
+  		elem.button('option', 'icons', {primary:'ui-icon-circle-plus'});
+  		elem.parents('.group_header:first').removeClass('selected');
 	}
-  }
+ }
   
-  /**
-   * Run a suite of jqueryUI enhancements against an element and its contents
-   * @param {Object}elem
-   */
-  function enhanceUI(elem) {
-  	 // Buttonize
- $('.load', elem).button();
- $('button.group_select').button({icons:{primary:'ui-icon-circle-plus'},
+/**
+* Run a suite of jqueryUI enhancements against an element and its contents
+* @param {Object}elem
+*/
+function enhanceUI(elem) {
+	// Buttonize
+	$('.load', elem).button();
+	$('button.group_select').button({icons:{primary:'ui-icon-circle-plus'},
                                                       text:true});
- $('.accordion').accordion({
- 	collapsible : true,
-	active : false,
-	clearStyle : true
- });
-  
- $('#nav a.pop_toggle', elem).each(function(){
-$(this).data('pop', $(this).next('.pop'));
-$(this).bind('click', function(){
-	$($(this).data('pop')).dialog();
-		});
+	$('.accordion').accordion({
+	 	collapsible : true,
+		active : false,
+		clearStyle : true
 	 });
-  }
+  
+	 $('#nav a.pop_toggle', elem).each(function(){
+		 $(this).data('pop', $(this).next('.pop'));
+		 $(this).bind('click', function(){
+			 $($(this).data('pop')).dialog();
+		 });
+	 });
+}
 
 /**
  * Encode a sparql query for use against our endpoint proxy 
@@ -303,6 +316,8 @@ var params = {'mode':'native',
  */
 function getDSDs() {
     // Listing the dsds and datasets
+	// Show a loader
+	$('#content').html('<div class="loader"><img src="./images/loading.gif" /><br /><em>Retrieving Datacubes</em></div>')
 	
 	var query = 'SELECT ?dsd ?dsdlabel ?dsdcomment ?dataset ?datasetlabel ?datasetcomment WHERE {';
 	query += '?dsd a qb:DataStructureDefinition .';
@@ -353,13 +368,15 @@ function getDSDs() {
                 }
 			}
 		 });
-         
          // Build the content
 		 $('#subtitle').text('Available Datacubes');
          $('#content').html($('#comparisonsTemplate').tmpl(dsds));     
-		 
 		 // Add jqueryUI elements
 		 enhanceUI($('#content'));
+		 
+		 //Notify the event coordinator
+		 dispatcher.trigger('dsds_loaded_event');
+         
       }
     );
 }
@@ -389,11 +406,13 @@ function loadDSD(dsd, output) {
 	$.getJSON(endpoint, build_sparql_params(query, true), 
      function(data){
 		dsds[dsd].structure = data.results.bindings;
+		
+		// Record the choice of dsd in a url fragment
+		jQuery.bbq.pushState({'active_dsd':dsd});
+		
 	    output.html($('#dsdElementsTemplate').tmpl(dsds[dsd]));    
 		//Reveal the relevant load button
-	
 		//$('button.load[value='+dsd+']').show();
-		
 		// THE ABOVE ATTR SELECTOR DOESN'T WORK IN IE7-. Something like below needed
 		$.each($('.load'), function(i,v) {
 			v = $(v);
@@ -402,9 +421,9 @@ function loadDSD(dsd, output) {
 			}
 		});
 		
-		//$('<button class="load"></button>').attr('value', dsd).insertAfter(?);
-		 }
-     );
+		// Notify the dispatcher
+		dispatcher.trigger('structure_loaded_event');
+	});
 }
 
 /**
@@ -440,14 +459,14 @@ function loadObservations(dsd_obj){
 	
 	$.getJSON(endpoint, build_sparql_params(query, true),
 		function(data){
-			// Bindings
-			$('.group_select').live('click', function(event){
-				toggleGroupSelection($(this).tmplItem());
-				//prevent this click from opening the containing accordion
-	           
-	            event.stopImmediatePropagation();
-				event.preventDefault();	
-			});
+
+			//$('.group_select').live('click', function(event){
+			//	toggleGroupSelection($(this).tmplItem());
+			//	//prevent this click from opening the containing accordion
+	        //   
+	        //    event.stopImmediatePropagation();
+			//	event.preventDefault();	
+			//});
 			
 			$.each(data.results.bindings, function (i, v){
 				dsds[dsd_obj.data.dsd_uri].observations.push(v);
@@ -461,10 +480,89 @@ function loadObservations(dsd_obj){
 			$('.group_select').click(function(event){
 	            toggleGroupSelection($(this).tmplItem());
 	            //prevent this click from opening the containing accordion
-	                event.stopImmediatePropagation();
-	                event.preventDefault();     
+	            event.stopImmediatePropagation();
+	            event.preventDefault();     
 	        });
+			
+			// Note that we've loaded observations 
+			jQuery.bbq.pushState({'load_observations':true});
+			
+			// Notify the dispatcher
+			dispatcher.trigger('observations_loaded_event');
 		});
+}
+
+/**
+ * Detect permalink fragments in the url and automate to suit
+ * Sorry about this implementation - don't read it if you don't have to
+ * @return
+ */
+function handle_permalinks() {
+	var perma = jQuery.deparam.fragment();	
+	if (perma.active_dsd) {
+		// Show a loading modal
+		var loader = $('<div class="loader" title="Loading Saved State"><img src="./images/loading.gif" /></div>').dialog({modal:true,  hide: {effect: "fadeOut", duration: 1000}});
+		dispatcher.bind('permalink_load_chain_complete', function(){ loader.dialog('close'); });
+
+		//Set up our loading chain
+		dispatcher.bind('dsds_loaded_event', function(){		
+			//Prepare the next chained listener to load observations
+			dispatcher.bind('structure_loaded_event', function(){
+				if (perma.load_observations) {
+					// Be ready to select items for comparison if needed
+					dispatcher.bind('observations_loaded_event', function(){
+						if (perma.select_groups) {
+							// Reverse the array so that we can easily build one in the same order
+							// without upsetting IE by use of unshift()
+							perma.select_groups.reverse();
+							$.each(perma.select_groups, function(i, v){
+								//Find the select button and trigger a click on it for each selected group
+								// Surely we could build selected_groups and then 
+								var ds = $('.dataset[title='+v.d+']');
+								var group = $('.group_header[title='+v.g+']', ds);
+								//$('.group_select', group).trigger('click');
+								var item = $('.group_select', group).tmplItem();
+								
+								//Last one - cause a ui update
+								if (i === perma.select_groups.length-1) {
+									toggleGroupSelection(item);
+								} else {
+									selected_groups.push(item);
+								}
+							});
+							// End of permalink chain
+							dispatcher.trigger('permalink_load_chain_complete');
+						
+						} else {
+							//There can be nothing more to do
+							dispatcher.trigger('permalink_load_chain_complete');
+						}
+					});
+
+					// fake up a templateItem
+					var dsd_obj = {'data':dsds[perma.active_dsd]}
+					loadObservations(dsd_obj);
+				} else {
+					dispatcher.trigger('permalink_load_chain_complete');
+				}
+			});
+			
+			// Trigger the accordion change on the element holding a ref to our active_dsd
+			var accordion_el = $('#dsds.accordion');
+			var active_index = null;
+			$('.dsd-toggle .uri', accordion_el).each(function(i,v){
+				if ($(v).text() == perma.active_dsd) {
+					active_index = i;
+				}
+			});
+			accordion_el.accordion('activate', active_index);
+		});
+		
+		//Start the load chain
+		getDSDs();
+	} else {
+		getDSDs();
+	}
 }
 		
 		
